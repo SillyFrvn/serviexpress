f42171bfa182cfcbe70fc82f4d085892f4f5c6ee
Franclas <sillyfrvn@gmail.com>
2023-12-14 11:02:15 -0300

    2.0

--- svexpress/app.py
+++ svexpress/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 
@@ -14,7 +14,53 @@ def mecanicos():
 def reserva():
     return render_template('toma_horas.html')
 
+@app.route('/miembros')
+def miembros():
+    return render_template('miembros.html')
+
+#LOGIN
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username]['password'] == password:
+            session['logged_in'] = True
+            session['username'] = username 
+            return redirect('/dashboard')
+        else:
+            return render_template('login.html', error='credenciales incorrectas')
+    return render_template('login.html')
+    
+#acceso al dashboard
+@app.route('/dashboard')
+def dashboard():
+    
+        return render_template('dashboard.html')
+    
+
+#ruta del logout
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+@app.route('/informe')
+def informe():
+    return render_template('informe.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+    #BASE DE DATOS 
+    users = {
+    'user1': {'username': 'admin', 'password': 'admin'},
+    'user2': {'username': 'user', 'password': 'user'}
+}
